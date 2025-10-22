2bf04703085ac0f4deb8b87c13864aa7b63578e1
Kinderek <sejudek12@gmail.com>
2025-10-22 14:46:28 +0200

    Test Biometrii

--- QuizGeograficzny/Views/LoginPage.xaml.cs
+++ QuizGeograficzny/Views/LoginPage.xaml.cs
@@ -1,3 +1,6 @@
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+
 namespace QuizGeograficzny.Views;
 
 public partial class LoginPage : ContentPage
@@ -7,8 +10,51 @@ public partial class LoginPage : ContentPage
         InitializeComponent();
     }
 
+
     private async void OnLoginClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("///difficulty");
+        try
+        {
+            
+            if (DeviceInfo.Platform == DevicePlatform.WinUI)
+            {
+                await Shell.Current.GoToAsync("///difficulty");
+                return;
+            }
+
+           
+            var availability = await CrossFingerprint.Current.IsAvailableAsync(true);
+
+            if (!availability)
+            {
+                await DisplayAlert("Uwaga", "Zalogowano testowo.", "OK");
+                await Shell.Current.GoToAsync("///difficulty");
+                return;
+            }
+
+            var authRequest = new AuthenticationRequestConfiguration(
+                "Uwierzytelnianie",
+                "Zaloguj siê odciskiem palca, aby rozpocz¹æ quiz.")
+            {
+                AllowAlternativeAuthentication = true
+            };
+
+            var result = await CrossFingerprint.Current.AuthenticateAsync(authRequest);
+
+            if (result.Authenticated)
+            {
+                await DisplayAlert("Sukces", "Pomyœlnie zalogowano!", "OK");
+                await Shell.Current.GoToAsync("///difficulty");
+            }
+            else
+            {
+                await DisplayAlert("Niepowodzenie", "Nie uda³o siê uwierzytelniæ.", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("B³¹d", $"Wyst¹pi³ wyj¹tek: {ex.Message}", "OK");
+        }
     }
+
 }
